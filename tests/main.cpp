b720b20bc2855dd5d09b7d6bcccb2c7a170fe472
smlu <smlu@s5.net>
2021-04-22 15:07:06 +0200

    [Test] Add sha256 intrinsic implementation

--- tests/main.cpp
+++ tests/main.cpp
@@ -1,12 +1,24 @@
-#include <ck/types.hpp>
+#include <eosio/tester.hpp>
+
 #include "keccak_test.hpp"
 #include "public_key_test.hpp"
+#include "sha2.hpp"
 
 using namespace ck::test;
+
+void init_test_intrinsics() {
+    using namespace eosio::native;
+    intrinsics::set_intrinsic<intrinsics::sha256>(
+    [](const char* data, uint32_t length, capi_checksum256* hash) {
+        auto d =  sha256( (const uint8_t*)data, length );
+        memcpy( hash->hash, d.data(), d.size() );
+    });
+}
 
 int main(int argc, char** argv)
 {
     silence_output(false);
+    init_test_intrinsics();
 
     EOSIO_TEST( keccak_test )
     EOSIO_TEST( public_key_test )
