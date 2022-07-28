ef40cbfb8f7ebdfbcfad8cc5ba2e331784d9da12
Crt Vavros - smlu <smlu@s5.net>
2022-07-28 11:38:43 +0200

    Refactor `copyf` param type

--- include/eosiock/mgf.hpp
+++ include/eosiock/mgf.hpp
@@ -9,17 +9,17 @@
 #include <eosiock/types.hpp>
 
 namespace eosiock {
-    template<typename Hash, typename CopyF = void*(*)(void *, const void *, size_t) >
-    void mgf1(const Hash& hashf, const bytes_view mgf_seed, const std::span<byte_t> mask, CopyF copyf = memcpy)
+    template<typename Hash, typename CopyF = void(*)(std::span<byte_t>, const bytes_view, size_t) >
+    void mgf1(const Hash& hashf, const bytes_view mgf_seed, std::span<byte_t> mask,
+              CopyF copyf = [](auto dst, auto src, auto len) { memcpy( dst.data(), src.data(), len ); })
     {
         eosio::check( mask.size() <= std::numeric_limits<uint32_t>::max(), "MGF1 mask too long" );
 
         uint32_t counter          = 0;
         std::size_t out_len       = mask.size();
-        byte_t* p_out_cur         = mask.data();
         const std::size_t buf_len = mgf_seed.size() + sizeof(counter);
         byte_t buf[buf_len];
-        while (out_len)
+        while ( out_len )
         {
             memcpy( buf, mgf_seed.data(), mgf_seed.size() );
 
@@ -33,10 +33,10 @@ namespace eosiock {
                 .extract_as_byte_array();
 
             const size_t copied = std::min<std::size_t>( hash.size(), out_len );
-            copyf( p_out_cur, hash.data(), copied );
+            copyf( mask, hash, copied );
 
-            p_out_cur += copied;
-            out_len   -= copied;
+            mask     = mask.subspan( copied );
+            out_len -= copied;
             counter++;
         }
     }
