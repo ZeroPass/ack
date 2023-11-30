bbe1eaa00c45287356c800dc6ef677c7374005d0
Crt Vavros - smlu <smlu@s5.net>
2023-11-30 22:20:28 +0100

    [Script] Fix secp256r1 prime number

--- scripts/ec.py
+++ scripts/ec.py
@@ -26,7 +26,7 @@ secp256k1 = EllipticCurve(
 secp256r1 = EllipticCurve(
     'secp256r1',
     # Field characteristic.
-    p=0xfffffffffffffffffffffffffffffffffffffffffffffffffffffffefffffc2f,
+    p=0xffffffff00000001000000000000000000000000ffffffffffffffffffffffff,
     # Curve coefficients.
     a=0xffffffff00000001000000000000000000000000fffffffffffffffffffffffc,
     b=0x5ac635d8aa3a93e7b3ebbd55769886bc651d06b0cc53b0f63bce3c3e27d2604b,
